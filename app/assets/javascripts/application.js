5921823e2dc385af8e1394db2f7f57e70d98adc5
Nainpreet <nainpreethunjan94@gmail.com>
2017-04-27 14:12:40 +0530

    Final Project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require twitter/bootstrap
 //= require bootstrap-datepicker
 //= require underscore
-//= require gmaps/google
 //= require_tree .
 $(document).ready(function(){
 
