a74bbd79ba7f6e80112700c8e9c13e7ab0c63d37
Nainpreet <nainpreethunjan94@gmail.com>
2017-05-01 15:02:43 +0530

    Final Project

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module AppointmentSystem
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = :local
+    # config.active_record.default_timezone = :local
   end
 end
