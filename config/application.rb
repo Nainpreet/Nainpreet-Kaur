5fc3338e2038994dff9db8c48f02e3720cb5be13
Nainpreet <nainpreethunjan94@gmail.com>
2017-05-01 17:07:40 +0530

    Final Project

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module AppointmentSystem
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'New Delhi'
-    # config.active_record.default_timezone = :UTC
+    config.active_record.default_timezone = :utc
   end
 end
