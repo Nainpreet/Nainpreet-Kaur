6793b8d2da5b49a9f5552c89df2800a4ecbe5235
Nainpreet <nainpreethunjan94@gmail.com>
2017-05-03 18:16:11 +0530

    Final Project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,15 +51,15 @@ $(document).ready(function(){
       current_fs = $(this).parent();
       next_fs = $(this).parent().next();
       // activate next step on progressbar
-      $("progressbar li").eq($("#msform fieldset").index(next_fs)).addClass("active");
+      $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
         //show next fieldset
         next_fs.show();
         //hide current fieldset
         current_fs.animate({opacity: 0},{
-          step: function(now,mx){
-              scale = 1 - (1 - now)* 0.2;
-              left = (now*50)+'%';
-              opacity = 1 - now;
+          step:function(now,mx){
+              scale = 1-(1-now)*0.2;
+              left = (now*50)+"%";
+              opacity = 1-now;
               current_fs.css({'transform':'scale('+scale+')'});
               next_fs.css({'left':left,'opacity':opacity});
             },
@@ -76,15 +76,15 @@ $(document).ready(function(){
       current_fs = $(this).parent();
       previous_fs = $(this).parent().prev();
       // activate next step on progressbar
-      $("progressbar li").eq($("#msform fieldset").index(current_fs)).removeClass("active");
+      $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
         //show next fieldset
         previous_fs.show();
         //hide current fieldset
         current_fs.animate({opacity: 0},{
-          step: function(now,mx){
-              scale = 0.8 + (1-now) * 0.2;
+          step:function(now,mx){
+              scale = 0.8+(1-now)*0.2;
               left = ((1-now)*50)+"%";
-              opacity = 1 - now;
+              opacity = 1-now;
               current_fs.css({'left':left});
               previous_fs.css({'transform':'scale('+scale+')','opacity':opacity});
             },
