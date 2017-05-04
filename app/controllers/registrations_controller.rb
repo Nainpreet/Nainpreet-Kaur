152a70bbe949f2bb3567b28bafeab7740763393e
Nainpreet <nainpreethunjan94@gmail.com>
2017-05-04 10:27:57 +0530

    remember me

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@
 class RegistrationsController < Devise::RegistrationsController
  before_filter :configure_permitted_parameters, only: [:update, :create]
  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :dob, :role, :address, :gender, :password, :password_confirmation])
    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :dob, :address, :gender, :password, :password_confirmation])
  end
